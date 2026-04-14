e7bc1a47695d1ed2a03841a13934d54c5bd895db
JCassio1 <Joselsondias@hotmail.com>
2026-04-14 23:04:36 +0100

    Initial commit - Rails Kafka order pipeline

---
+++ db/migrate/20260414213018_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[8.1]
+  def change
+    create_table :orders do |t|
+      t.string :description
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
